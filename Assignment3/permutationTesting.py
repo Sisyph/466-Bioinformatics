a1cef0f6d8c64815a48fd762e60df95e268a3fe0
Joseph Betz <josephbetz@Joseph-Betzs-MacBook.local>
2014-03-31 21:58:40 -0500

    implemented fasta parser for permutationTesting.py

--- Assignment3/permutationTesting.py
+++ Assignment3/permutationTesting.py
@@ -1,2 +1,22 @@
-# Assignment 2, Problem 3
-# Permutation-based tests of significance
+def SimpleFastaParser(fileName):
+    handle = open(fileName)
+    line = handle.readline()
+    while True:
+        title = line[1:].rstrip()
+        lines = []
+        line = handle.readline()
+        while True:
+            if not line:
+                break
+            if line[0] == ">":
+                break
+            lines.append(line.rstrip())
+            line = handle.readline()
+            print line
+        if not line:
+            return
+
+def main(argv):
+    parsedFastaFile = SimpleFastaParser(fastaFileName)
+    print parsedFastaFile
+    return
