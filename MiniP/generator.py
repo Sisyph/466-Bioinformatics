df3b56746a35374cd3ebca2d9288da4a53649d10
Sisyph <Sisyph@users.noreply.github.com>
2014-04-29 23:43:26 -0500

    new

--- MiniP/generator.py
+++ MiniP/generator.py
@@ -9,24 +9,99 @@ def genRandBase():
 
 
 #check filename 
-def create_dir(filename):
-	d = os.path.dirname(filename)
-	try:
-   		os.stat(d)
-	except:
-  		os.mkdir(d) 
+def create_dir(newpath):
+	if not os.path.exists(newpath): os.makedirs(newpath)
+
+#makes k dirs with the given parameters
+def dir_params(ML,NM,SC,k):
+	for run in range(0,k):
+		create_dir("data/set_"+str(ML)+"_"+str(NM)+"_"+str(SC)+"/_"+str(run)) 
+
+#specifies the different data directories for each of the paramter sets
+def make_subdirs():
+	create_dir("data")
+	dir_params(8,1,10,10)
+	dir_params(8,0,10,10)
+	dir_params(8,2,10,10)
+	dir_params(6,1,10,10)
+	dir_params(7,1,10,10)
+	dir_params(8,1,5,10)
+	dir_params(8,1,20,10)
+	
+#
+def make_seqs(SL, SC):
+	output = []	
+	for x in range(0,SC):
+		this_seq = ""
+		for j in range(0,SL):
+			this_seq += genRandBase()	
+		output.append(this_seq)
+
+	return output
+
+#is biased to place stars near beginning of motif
+def gen_motif(ML,NM):
+	output = ""
+	inputs = range(ML)
+	stars = []
+	count = 0
+	while (count< NM):
+		tempStar = random.choice(inputs)
+		stars.append(tempStar)
+		inputs.remove(tempStar)		
+		count+=1
+
+	for x in range(0,ML):
+		if (x in stars):
+			output+= '*'
+			
+		else:
+			output += genRandBase()
+	return output
+
+#
+def gen_bind_sites(motif,SC):
+	output = []
+	for x in range(0,SC):
+		temp = ""
+		for pos in list(motif):
+			if (pos == '*'):
+				temp +=genRandBase()
+			else: 
+				temp += pos
+		output.append(temp)
+	return output	
+
+#choses bind sites in same order as seqs, maybe want to randomize
+def genBoundSeqs(seqs, bind_sites, ML, SL):
+	output = []
+	for x in range(len(seqs)):
+		pos = random.randint(0,SL-ML)
+		output.append(seqs[x][0:pos] + bind_sites[x] + seqs[x][pos+ML:])
+	return output
+
+#pass in params to generate all the sequences and files for those params
+def generator(ML,NM,SL,SC):
+	pDir = "data/set_"+str(ML)+"_"+str(NM)+"_"+str(SC)
+	for run in range(0,10):
+		subDir = pDir + "/_"+str(run) 
+		seqs = make_seqs(SL,SC)		#array of strings
+		motif = gen_motif(ML,NM)	#string
+		bind_sites = gen_bind_sites(motif,SC)	#array of strings
+		boundSeqs = genBoundSeqs(seqs, bind_sites, ML, SL)	#array of strings
 
 
 if __name__ == "__main__":
 	print "hello generator"
+	make_subdirs()
+	generator(8,1,50,10)
 
-
-
-
+	#7 calls to generator
 
 	
 #create a directory /data/
-#create 10 subidrectories of /data/ for each param list wiht format /data/set_$ML_$NM_$SC/_$run 	(example: /data/set_8_1_10/_3 ) 
+#create 10 subidrectories of /data/ for each param list wiht 
+#format /data/set_$ML_$NM_$SC/_$run 	(example: /data/set_8_1_10/_3 ) 
 #do the following for each set of parameter values 10 times and put the output in appropriate folders there are 70 lowest lvl sub directories which will contain 4 files each
 
 #generate an array SC long of sequences(strings) of length SL
