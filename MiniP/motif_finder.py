ef4222c31339101bf1fcec4e20a21e424832583c
Joseph Betz <josephbetz@mobile-192-17-206-54.near.uiuc.edu>
2014-04-29 19:04:33 -0500

    created data directory

--- MiniP/motif_finder.py
+++ MiniP/motif_finder.py
@@ -1,5 +1,18 @@
+import sys
+#import reader
+#import writer
+from os import listdir
+from os.path import isfile, join
+
 if __name__ == "__main__":
 	print "hello motif"
+        directoryPath = "./data/"
+        dataFiles = [ files for file in listdir(directoryPath) if isfile(join (mypath, f)) ]
+        print dataFiles
+        #fastaFile = readers.readFastaFile(fastaFileName)
+        #motifLength = readers.readMotifLengthFile(motifLengthFileName)
+        #motifs = findMotifs(fastaFile, motifLength)
+        
 
 #for each of the 70 subdirectories does the following
 #reads in motiflength.txt and sequences.fa
