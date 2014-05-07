1368ee0dc1774b0a2d61511de98ad55ea726565e
Sisyph <Sisyph@users.noreply.github.com>
2014-05-06 22:48:44 -0500

    added comments a bit

--- MiniP/evaluation.py
+++ MiniP/evaluation.py
@@ -107,6 +107,4 @@ if __name__ == "__main__":
 
 	print "evaluation finished"
 
-#for each of the 70 subdirectories
-#compute the metrics
-#perhaps generate graphs, though this could be done externally if the metrics are output to csv or similar
+
